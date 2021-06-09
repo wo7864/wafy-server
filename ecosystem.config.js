724be7f605a60aa47d6e1fb20702ad7eecfd97d4
wo7864 <wo78644@gmail.com>
2021-06-10 05:10:13 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,9 @@ module.exports = {
     apps: [
         {
             name: "wafy", // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
-            script: "/home/ubuntu/wafy-server/dist/index.js", // pm2로 실행될 파일 경로
+            script: "./dist", // pm2로 실행될 파일 경로
             watch: true, // 파일이 변경되면 자동으로 재실행 (true || false)
-            node_args:"-r module-alias/register --env=production",
+            node_args:["-r module-alias/register", "--env=production"],
             env: {
                 "env": "development" // 개발환경시 적용될 설정 지정
             },
