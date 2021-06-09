9df5bf2719a74adfe3edf79dc339b30240774d1f
wo7864 <wo78644@gmail.com>
2021-06-10 04:57:49 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,7 @@ module.exports = {
             name: "wafy", // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
             script: "/home/ubuntu/wafy-server/dist/index.js", // pm2로 실행될 파일 경로
             watch: true, // 파일이 변경되면 자동으로 재실행 (true || false)
+            node_args:"-r module-alias/register --env=production",
             env: {
                 "env": "development" // 개발환경시 적용될 설정 지정
             },
@@ -13,26 +14,6 @@ module.exports = {
                 "env": "production", // 배포환경시 적용될 설정 지정,
                 "NODE_ENV": "production",
 
-
-                "PORT": 8081,
-                "HOST": "localhost",
-
-
-                "JET_LOGGER_MODE": "FILE",
-                "JET_LOGGER_FILEPATH": "jet-logger.log",
-                "JET_LOGGER_TIMESTAMP": "TRUE",
-                "JET_LOGGER_FORMAT": "LINE",
-
-                "COOKIE_DOMAIN": "localhost",
-                "COOKIE_PATH": "/",
-                "SECURE_COOKIE": "false",
-                "JWT_SECRET": "xxxxxxxxxxxxxx",
-                "COOKIE_SECRET": "xxxxxxxxxxxxxx",
-                "COOKIE_EXP": 259200000,
-
-
-                "HOST_EMAIL": "wo7864@gmail.com",
-                "HOST_EMAIL_PW": "3kat59!@",
 
             }
         }
